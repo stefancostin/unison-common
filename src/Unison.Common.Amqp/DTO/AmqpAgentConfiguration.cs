64c3d48cf4c4195964828d8db721765c05d3dd54
Stefan Costin <stefanfcostin@gmail.com>
2021-07-05 09:40:43 +0300

    Add heartbeat dto

---
+++ src/Unison.Common.Amqp/DTO/AmqpAgentConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unison.Common.Amqp.DTO
+{
+    public class AmqpAgentConfiguration : AmqpMessage
+    {
+        public int HeartbeatTimer { get; set; }
+    }
+}
